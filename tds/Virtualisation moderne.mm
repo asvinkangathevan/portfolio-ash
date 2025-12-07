d481d1609f6c5d2776680f35829182d0ea4efb1f
Ashwin <asvinkangathevan@gmail.com>
2025-12-07 12:58:46 +0100

    Add files via upload

---
+++ tds/Virtualisation moderne.mm
@@ -0,0 +1,116 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks>
+    <bookmark nodeId="ID_696401721" name="Racine" opensAsRoot="true"/>
+</bookmarks>
+<node TEXT="Virtualisation moderne" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1765108347873" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" auto_compact_layout="true" show_tags="UNDER_NODES" associatedTemplateLocation="template:/standard-1.6.mm" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="14" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Events" POSITION="bottom_or_right" ID="ID_1002889146" CREATED="1759304376405" MODIFIED="1759305371580" HGAP_QUANTITY="-10 pt" VSHIFT_QUANTITY="-49.5 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Big2025" ID="ID_1506031280" CREATED="1759305422587" MODIFIED="1759305445370" VSHIFT_QUANTITY="-36 pt"/>
+<node TEXT="CTF" ID="ID_916826514" CREATED="1759305450620" MODIFIED="1759305471438"/>
+<node TEXT="Salm" ID="ID_447761085" CREATED="1759305480130" MODIFIED="1759305537641" VSHIFT_QUANTITY="22.5 pt"/>
+</node>
+<node TEXT="" POSITION="bottom_or_right" ID="ID_1637795427" CREATED="1759305406699" MODIFIED="1759305406699">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Définition" POSITION="top_or_left" ID="ID_1994275549" CREATED="1759304409021" MODIFIED="1759304436296">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Lieux" POSITION="bottom_or_right" ID="ID_769990551" CREATED="1759304441647" MODIFIED="1759305200480" HGAP_QUANTITY="53 pt" VSHIFT_QUANTITY="-37.5 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="" ID="ID_1619306284" CREATED="1759304530017" MODIFIED="1759305307911" HGAP_QUANTITY="47 pt" VSHIFT_QUANTITY="-45.75 pt">
+<node TEXT="" ID="ID_682661946" CREATED="1759305251974" MODIFIED="1759305278940"/>
+</node>
+<node TEXT="Campus" ID="ID_1770597626" CREATED="1759305337726" MODIFIED="1765108555526"/>
+</node>
+<node TEXT="Technologies" POSITION="top_or_left" ID="ID_1196064247" CREATED="1759304502580" MODIFIED="1759304526568" VSHIFT_QUANTITY="38.25 pt">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Kali Linux" ID="ID_1723879930" CREATED="1759304681044" MODIFIED="1765108332291" HGAP_QUANTITY="26 pt" VSHIFT_QUANTITY="28.5 pt"/>
+</node>
+<node TEXT="News" POSITION="top_or_left" ID="ID_1046410359" CREATED="1759304936184" MODIFIED="1765108361199" HGAP_QUANTITY="-6.25 pt" VSHIFT_QUANTITY="89.25 pt">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Gestion de la sécurité dans la cadre d&apos;un système d&apos;information" POSITION="bottom_or_right" ID="ID_1955324011" CREATED="1759305282075" MODIFIED="1759306048627" VSHIFT_QUANTITY="10.5 pt">
+<edge COLOR="#7c7c00"/>
+</node>
+<node TEXT="Site d&apos;informatique" POSITION="bottom_or_right" ID="ID_444352859" CREATED="1759305312083" MODIFIED="1759306044964" HGAP_QUANTITY="-41.5 pt" VSHIFT_QUANTITY="46.5 pt">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="" LOCALIZED_STYLE_REF="defaultstyle.floating" POSITION="bottom_or_right" ID="ID_1308511514" CREATED="1765108534987" MODIFIED="1765108535039" HGAP_QUANTITY="150.75 pt" VSHIFT_QUANTITY="-10.5 pt">
+<hook NAME="FreeNode"/>
+</node>
+</node>
+</map>
